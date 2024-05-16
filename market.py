baef8aacc558d76a5ecc191d8140abad58e215e0
Abdul Waris <maiyaleeg223@gmail.com>
2024-05-16 16:39:57 -0400

    merger

--- market.py
+++ market.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# home route
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template("home.html")
+
+# shop route
+@app.route('/shop')
+def shop_page():
+    return render_template('market.html')
