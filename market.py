79fafdbf80f82cdde464a825ed7251bd2aac09cf
Abdul Waris <maiyaleeg223@gmail.com>
2024-05-17 08:45:24 -0400

    Added a db

--- market.py
+++ market.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=20), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=500), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 
 # home route
 @app.route('/')
