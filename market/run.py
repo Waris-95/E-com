8f4eae858138ddd5a17d8f0119cec074696c4dba
Abdul Waris <maiyaleeg223@gmail.com>
2024-05-22 21:24:30 -0400

    got the route to work again

--- market/run.py
+++ market/run.py
@@ -1,4 +1,6 @@
-from market import app
+from market import app, db
 
 if __name__ == '__main__':
-    app.run()
+    with app.app_context():
+        db.create_all()  # Ensure all tables are created
+    app.run(debug=True)
