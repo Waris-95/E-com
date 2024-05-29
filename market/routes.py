5a56e771d0eaf6d0487e459fd1d041ec4bebd527
Abdul Waris <maiyaleeg223@gmail.com>
2024-05-29 13:37:33 -0400

    Adding signup

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template, jsonify
 from market.models import Item
+from market.forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -18,3 +19,8 @@ def get_items():
     items = Item.query.all()
     items_list = [{'name': item.name, 'price': item.price, 'barcode': item.barcode, 'description': item.description} for item in items]
     return jsonify(items_list)
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
