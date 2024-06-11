b8a909b5bf898059be71e2bb437dbdd494141d6f
Abdul Waris <maiyaleeg223@gmail.com>
2024-06-11 13:55:06 -0400

    testing prod db

--- market/__init__.py
+++ market/__init__.py
@@ -6,19 +6,20 @@ import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from sqlalchemy import text
+import logging
+from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-# Database configuration for development and production
 if os.getenv('FLASK_ENV') == 'production':
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
-    app.config['DEBUG'] = True  # Add this line to enable debugging
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-    app.config['DEBUG'] = True  # Add this line to enable debugging
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -30,7 +31,6 @@ def load_user(user_id):
     from market.models import User  # Import here to avoid circular import
     return User.query.get(int(user_id))
 
-# to render a better 401
 @app.errorhandler(401)
 def unauthorized(e):
     return render_template('401.html'), 401
@@ -43,9 +43,15 @@ def set_schema():
             with db.engine.connect() as connection:
                 connection.execute(text(f'SET search_path TO {schema};'))
 
-# Import routes after initializing app, db, and other components to avoid circular import
 from market import routes
 
-# Register commands
 from market.seed import seed
 app.cli.add_command(seed)
+
+if not app.debug:
+    file_handler = RotatingFileHandler('error.log', maxBytes=10240, backupCount=10)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
